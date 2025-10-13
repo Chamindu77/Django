4b98736186b457b10e16fe49398d3d3d30494527
chamindu77 <chamindunipun99@gmail.com>
2025-10-14 00:17:26 +0530

    Add initial book data and templates for bookstore project
    - Created books.json with detailed book entries including title, authors, descriptions, and categories.
    - Added initial migration for Book model with title field.
    - Extended Book model with additional fields: longDescription, pageCount, shortDescription, and thumbnailUrl.
    - Implemented index.html template to display a grid of books with titles and thumbnails.
    - Developed show.html template for individual book details, including title, publication date, page count, categories, authors, and descriptions.

--- bookstore-project/books/models.py
+++ bookstore-project/books/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+ 
+class Book(models.Model):
+    title = models.CharField(max_length=256)
+    pageCount = models.IntegerField(default=0)
+    thumbnailUrl = models.CharField(max_length=512, null=True)
+    shortDescription = models.CharField(max_length=256, null=True)
+    longDescription = models.TextField(null=True)
+    # authors = models.ManyToManyField(Author)
+    # image = models.ImageField(upload_to="images", null=True)
+
+    def __str__(self):
+        return f"{self.id} {self.title}"
+        
--- bookstore-project/books/views.py
+++ bookstore-project/books/views.py
@@ -1,6 +1,40 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from books.models import Book
+from django.http import Http404
+
+# import json
+# import os
+ 
+# # Get the base directory of your project
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# # Build the correct path to books.json
+# books_path = os.path.join(BASE_DIR, 'books.json')
+
+# # Open JSON file safely
+# with open(books_path, encoding='utf-8') as f:
+#     data = json.load(f)
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    dbData = Book.objects.all()
+    context = {'books': dbData}
+    return render(request, 'books/index.html', context)
+
+
+def show(request, id):
+
+    singleBook = get_object_or_404(Book, id=id)
+    
+    context = {'book': singleBook}
+    return render(request, 'books/show.html', context)
+
+# def show(request, id):
+
+#     singleBook = list()
+#     for book in data:
+#         if book['id'] == id:
+#             singleBook = book
+
+#     context = {'book': singleBook}
+#     return render(request, 'books/show.html', context)
+
